b19b47b1679bd1d02fa3e92398895a836023c7f2
vuvietbach <vuvietbach12345@gmail.com>
2023-06-22 14:09:22 +0700

    add dotenv

--- src/models/index.js
+++ src/models/index.js
@@ -1,25 +1,42 @@
 "use strict";
+require("dotenv").config();
 
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
-const process = require("process");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.json")[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
-    config
-  );
+console.log(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD);
+const sequelize = new Sequelize.Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOSTNAME,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    dialect: "mysql",
+    logging: false,
+  }
+);
+async function connectToDatabase() {
+  try {
+    await sequelize.authenticate();
+    console.log(
+      "Connection to the database has been established successfully."
+    );
+    // Perform other operations or start your application
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 }
+
+connectToDatabase();
 
 fs.readdirSync(__dirname)
   .filter((file) => {
